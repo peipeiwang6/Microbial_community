3d73de50bda731f66fbe3bea22f3b827ca748933
Peipei Wang <peipeiwang6@gmail.com>
2020-06-15 09:18:23 -0400

    Update 01_make_matrix_for_ML_add_fertstat.py

--- 01_make_matrix_for_ML_add_fertstat.py
+++ 01_make_matrix_for_ML_add_fertstat.py
@@ -5,10 +5,10 @@ Location = {}
 for inl in feature[1:]:
 	tem = inl.strip().split()
 	if tem[3] == 'Fert':
-		F[tem[0]] = '1\t' + '\t'.join(tem[6:66])
+		F[tem[0]] = '1\t' + '\t'.join(tem[6:86])
 	if tem[3] == 'Unfert':
-		F[tem[0]] = '0\t' + '\t'.join(tem[6:66])
-	Location[tem[0]] = [tem[2],tem[3],tem[66],tem[67],tem[68]]
+		F[tem[0]] = '0\t' + '\t'.join(tem[6:86])
+	Location[tem[0]] = [tem[2],tem[3],tem[86],tem[87],tem[88]]
 
 diversity = open('Rarefied_diversity_MMPRNT_G5_LC_LUX_016017.txt','r').readlines()
 D = {}
@@ -17,7 +17,7 @@ for inl in diversity[1:]:
 	D[tem[0]] = tem[1:]
 	
 out = open('Matrix_full_Richness.txt','w')
-title = 'sampleID\tY\tFertstat\t'+ '\t'.join(feature[0].split()[6:66])
+title = 'sampleID\tY\tFertstat\t'+ '\t'.join(feature[0].split()[6:86])
 out.write(title + '\n')
 for id in D:
 		out.write('%s\t%s\t%s\n'%(id,D[id][0],F[id]))
@@ -25,7 +25,7 @@ for id in D:
 out.close()
 
 out = open('Matrix_full_InvSimpson.txt','w')
-title = 'sampleID\tY\tFertstat\t'+ '\t'.join(feature[0].split()[6:66])
+title = 'sampleID\tY\tFertstat\t'+ '\t'.join(feature[0].split()[6:86])
 out.write(title + '\n')
 for id in D:
 		out.write('%s\t%s\t%s\n'%(id,D[id][1],F[id]))
@@ -33,11 +33,13 @@ for id in D:
 out.close()
 
 out = open('Matrix_full_Evenness.txt','w')
-title = 'sampleID\tY\tFertstat\t'+ '\t'.join(feature[0].split()[6:66])
+title = 'sampleID\tY\tFertstat\t'+ '\t'.join(feature[0].split()[6:86])
 out.write(title + '\n')
 for id in D:
 		out.write('%s\t%s\t%s\n'%(id,D[id][2],F[id]))
 		
 out.close()
+
+
 
 
