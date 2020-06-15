2559e2cc00a4332624970d43eccef1f09aee241c
Peipei Wang <peipeiwang6@gmail.com>
2020-06-15 09:21:08 -0400

    Update 01_make_matrix_for_ML_add_fertstat.py

--- 01_make_matrix_for_ML_add_fertstat.py
+++ 01_make_matrix_for_ML_add_fertstat.py
@@ -1,14 +1,15 @@
 import sys,os
 feature = open('metadata_full_MMPRNT_G5.LC_LUX_trunc_rar_016017_locations.txt','r').readlines()
 F = {}
+ncol = len(feature[0].split())
 Location = {}
 for inl in feature[1:]:
 	tem = inl.strip().split()
 	if tem[3] == 'Fert':
-		F[tem[0]] = '1\t' + '\t'.join(tem[6:86])
+		F[tem[0]] = '1\t' + '\t'.join(tem[6:(ncol-3)])
 	if tem[3] == 'Unfert':
 		F[tem[0]] = '0\t' + '\t'.join(tem[6:86])
-	Location[tem[0]] = [tem[2],tem[3],tem[86],tem[87],tem[88]]
+	Location[tem[0]] = [tem[2],tem[3],tem[ncol-3],tem[ncol-2],tem[ncol-1]]
 
 diversity = open('Rarefied_diversity_MMPRNT_G5_LC_LUX_016017.txt','r').readlines()
 D = {}
@@ -17,7 +18,7 @@ for inl in diversity[1:]:
 	D[tem[0]] = tem[1:]
 	
 out = open('Matrix_full_Richness.txt','w')
-title = 'sampleID\tY\tFertstat\t'+ '\t'.join(feature[0].split()[6:86])
+title = 'sampleID\tY\tFertstat\t'+ '\t'.join(feature[0].split()[6:(ncol-3)])
 out.write(title + '\n')
 for id in D:
 		out.write('%s\t%s\t%s\n'%(id,D[id][0],F[id]))
@@ -25,7 +26,7 @@ for id in D:
 out.close()
 
 out = open('Matrix_full_InvSimpson.txt','w')
-title = 'sampleID\tY\tFertstat\t'+ '\t'.join(feature[0].split()[6:86])
+title = 'sampleID\tY\tFertstat\t'+ '\t'.join(feature[0].split()[6:(ncol-3)])
 out.write(title + '\n')
 for id in D:
 		out.write('%s\t%s\t%s\n'%(id,D[id][1],F[id]))
@@ -33,7 +34,7 @@ for id in D:
 out.close()
 
 out = open('Matrix_full_Evenness.txt','w')
-title = 'sampleID\tY\tFertstat\t'+ '\t'.join(feature[0].split()[6:86])
+title = 'sampleID\tY\tFertstat\t'+ '\t'.join(feature[0].split()[6:(ncol-3)])
 out.write(title + '\n')
 for id in D:
 		out.write('%s\t%s\t%s\n'%(id,D[id][2],F[id]))
